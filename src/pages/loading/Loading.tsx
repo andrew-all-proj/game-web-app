3a58a4e24d4221d42faae9aa5f55001865f013e0
Andrew <andrew.publik.mail@gmail.com>
2025-07-08 12:02:28 +0300

    added loding and lottie

--- src/pages/loading/Loading.tsx
+++ src/pages/loading/Loading.tsx
@@ -1,15 +1,17 @@
-import styles from './Loading.module.css'
 import { observer } from 'mobx-react-lite'
+import Lottie from 'lottie-react'
+import loadingAnimation from '../../assets/lottie/loading_eye_monstr.json'
+import styles from './Loading.module.css'
 import label from '../../assets/icon/label-icon.svg'
 
 const Loading = observer(() => {
   return (
-    <>
-      <div className={styles.loadingPage}>
-        <div className={styles.spinner} />
-        <img src={label} alt="Mutantorium" className={styles.labelIcon} />
+    <div className={styles.loadingPage}>
+      <div className={styles.lottieWrapper}>
+        <Lottie animationData={loadingAnimation} loop autoplay />
       </div>
-    </>
+      <img src={label} alt="Mutantorium" className={styles.labelIcon} />
+    </div>
   )
 })
 
